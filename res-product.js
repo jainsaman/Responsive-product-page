78916a58205e24c82bd737611a7894c280072c6f
Saman Jain <81140577+jainsaman@users.noreply.github.com>
2021-05-31 16:57:39 +0530

    Update res-product.js

--- res-product.js
+++ res-product.js
@@ -1,10 +1,18 @@
+var x = window.innerWidth;
+
 function openNav(){
-    document.getElementById("mysidenav").style.width = "25%";
-    console.log("Hello");
+    if(x > 768){
+        document.getElementById("mysidenav").style.width = "25%";
+    } else {
+        document.getElementById("mysidenav").style.width = "50%";
+        console.log('Hello');
+    }
 }
+
 function closeNav(){
     document.getElementById("mysidenav").style.width = "0";
 }
+
 var buttons = document.getElementsByClassName("sizebut");
 for(let i=0 ; i<6 ; i++){
     buttons[i].addEventListener('click', function(){
